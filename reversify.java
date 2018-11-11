9f2f64c9a81371aedfde9f736da1b95c8307a77e
Sarah Oh <40012392+sarahoh2468@users.noreply.github.com>
2018-11-11 03:20:30 -0500

    Add files via upload

--- reversify.java
+++ reversify.java
@@ -1,34 +1,67 @@
 import com.sun.imageio.plugins.gif.GIFImageReader;
 import com.sun.imageio.plugins.gif.GIFImageReaderSpi;
-
 import javax.imageio.ImageIO;
 import javax.imageio.ImageReader;
 import java.awt.image.BufferedImage;
 import java.io.*;
 import java.util.ArrayList;
-public class reversify {
+import java.util.Scanner;
 
-    public static ArrayList<BufferedImage> getFrames(File gif) throws IOException{
+public class reversify {
+    public static ArrayList<BufferedImage> getTotalFrames(File gif) throws IOException {
         ArrayList<BufferedImage> frames = new ArrayList<BufferedImage>();
         ImageReader ir = new GIFImageReader(new GIFImageReaderSpi());
         ir.setInput(ImageIO.createImageInputStream(gif));
-        for(int i = 0; i < ir.getNumImages(true); i++)
+        for (int i = 0; i < ir.getNumImages(true); i++) {
             frames.add(ir.getRawImageType(i).createBufferedImage(ir.getWidth(i), ir.getHeight(i)));
+        }
         return frames;
     }
 
     public static int numberOfFrames(ArrayList frames) {
-        int i = frames.size();
+        int i = frames.size() / 10;
         return i;
     }
 
-    public static void main(String[] args) throws IOException {
-        //Scanner sc = new Scanner(System.in);
-        //String s = sc.next();
-        File file = new File("C:\\cat.gif");
-        ArrayList frames = getFrames(file);
-        int n = numberOfFrames(frames);
-        System.out.println(n);
+    public static ArrayList<BufferedImage> getFrames(ArrayList<BufferedImage> old) throws IOException {
+        int i = numberOfFrames(old);
+        ArrayList<BufferedImage> frames = new ArrayList<BufferedImage>(i);
+        for (int j = 0; j < old.size(); j += i) {
+            frames.add(old.get(j));
+        }
+        return frames;
+    }
 
+    public static void main(String[] args) throws IOException {
+        //keywords represent the title or frequent words that came up in the search
+        ArrayList<String> keywords = new ArrayList<String>();
+        Scanner sc = new Scanner(System.in);
+        File file = new File(sc.next());
+        ArrayList<BufferedImage> totalFrames = getTotalFrames(file);
+        ArrayList<BufferedImage> result = getFrames(totalFrames);
+        System.out.println(numberOfFrames(result));
+        // for each image, there is a keyword(s) assigned
+        for (int i = 0; i < result.size(); i++) {
+            //search api for image
+            // return keyword to keywords[i]
+        }
+        // for every keyword, count how many times it occurred in the set
+        for (int i = 0; i < keywords.size(); i++) {
+            for (int j = 0; j < keywords.size(); j++) {
+                if (keywords[i] == keywords[j]) {
+                    keywords[i][0] += 1;
+                }
+            }
+        }
+        // find the keyword(s) that occurred the most
+        int largest=0;
+        String s;
+        for (int i = 0; i < keywords.size(); i++) {
+            if (keywords[i][0] > largest) {
+                largest = keywords[i][0];
+                s = keywords[i];
+            }
+        }
+        System.out.println(s);
     }
 }
